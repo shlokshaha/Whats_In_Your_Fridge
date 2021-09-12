5fc4d2b68b5d95291e3f67e35e0b5dea1e04fcb4
shlokshaha <shlokshaha1421@gmail.com>
2021-09-12 19:45:44 +0530

    logout ui fixed

--- WhatsInYourFridge/settings.py
+++ WhatsInYourFridge/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*e_%vb-dto65($utgdv2==)a*jaytco92jmem_ui1zfh5$_af!'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
