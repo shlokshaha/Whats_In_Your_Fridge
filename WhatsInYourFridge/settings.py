ceb067f6d4d0a6d00b8f288aa7f0244c32f3b5e4
shlokshaha <shlokshaha1421@gmail.com>
2021-11-18 22:32:23 +0530

    modified settings.py

--- WhatsInYourFridge/settings.py
+++ WhatsInYourFridge/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '*e_%vb-dto65($utgdv2==)a*jaytco9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['whats-in-ur-fridge.herokuapp.com', '127.0.0.1']
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')     #heroku
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')     #heroku
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
